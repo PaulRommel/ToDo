a10af5e304480fe2c26400014612afaafee4b1b4
Pavel Popov <pavelpopov@MacBook-Pro-Pavel.local>
2025-05-05 21:08:24 +0300

    Create Project

---
+++ Task+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Task+CoreDataClass.swift
+//  ToDo
+//
+//  Created by Pavel Popov on 05.05.2025.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Task)
+public class Task: NSManagedObject {
+
+}
